d9aa5622d8fb4e7749d1e71d89bdf0fb97dcf8a3
vboxuser <vboxuser@Debian.myguest.virtualbox.org>
2023-01-23 20:38:12 +0100

    Thread Creation Benchmark

---
+++ IntroductionToThreads/FalseSharing/ThreadCreationBenchmark.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
+#include <random>
+#include <chrono>
+
+class TimeMe {
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_;
+    std::string label_;
+    static std::mutex mtx_;
+public:
+    TimeMe(const std::string& label) : label_(label), start_(std::chrono::high_resolution_clock::now()) {}
+    ~TimeMe(){
+        auto stop = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start_);
+        std::unique_lock<std::mutex> lock(TimeMe::mtx_);
+        std::cout << label_ << " = " << duration.count() << " us" << std::endl;
+    }  
+};
+std::mutex TimeMe::mtx_;
+
+std::vector<int> getRandomData(const int size){
+    std::vector<int> output(size);
+    std::generate(output.begin(), output.end(), std::rand);
+    return output;
+}
+
+void sorter(std::vector<int>& vect, const int start, const int stop, const int id)
+{
+    auto timer = TimeMe("Time spent in worker " + std::to_string(id));
+    std::sort(vect.begin() + start, vect.begin() + stop);
+}
+
+std::vector<int> sequentialSorter(std::vector<int> vect){
+    auto timer = TimeMe("Time spent on sequential sorting");
+    std::sort(vect.begin(), vect.end());
+    return vect;
+}
+
+
+
+int main(){
+    constexpr int iterations = 100000;
+    auto timer = TimeMe("Iterations (" + std::to_string(iterations)+")");
+    auto lambda = [](){};
+    for (auto i = 0; i < iterations; ++i)
+    {
+        auto th = std::thread(lambda);
+        th.detach();
+    }
+    return 0;
+}
