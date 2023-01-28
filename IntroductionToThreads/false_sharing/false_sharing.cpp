83986f03d497f0522d409791dc4bd0f9a63867b1
vboxuser <vboxuser@Debian.myguest.virtualbox.org>
2023-01-28 20:35:35 +0100

    some ammends to false sharing - comparing with trivial implementation of accumulate, some other changes as well

--- IntroductionToThreads/false_sharing/false_sharing.cpp
+++ IntroductionToThreads/false_sharing/false_sharing.cpp
@@ -40,11 +40,20 @@ struct UnalignedType {
     UnalignedType(type value) : val(value) {}
 };
 
+/* std::accumulate with -O0 is a tad slow, for fair comparision between
+ * single and multi-threaded accumulate, the below code is provided */
+template <class type>
+int accumulate(const std::vector<type>& input) {
+	type accu{0};
+	for (auto i = 0ul; i < input.size(); ++i) accu += input[i];
+	return accu;
+}
+
 template <class alignmentType, class elementType>
 class FalseSharingAccumulator {
 private:
     std::size_t numThreads_;
-    void partialSum(std::vector<elementType>& input, const std::size_t start, const std::size_t stop, elementType& accumulator) {
+    void partialSum(const std::vector<elementType>& input, const std::size_t start, const std::size_t stop, elementType& accumulator) {
         std::size_t lastElement = stop;
         if (input.size() < stop ) lastElement = input.size();
         for (auto i = start; i < lastElement; ++i ) accumulator += input[i];
@@ -55,7 +64,7 @@ public:
     {
         /* partialProduct is a vector containing per thread accumulators */
         std::vector<alignmentType> partialProduct(numThreads_,0);
-        std::cout << "The output structure is aligned to: " << sizeof(alignmentType(0)) << " bytes" << std::endl;
+        std::cout << "\nThe output structure is aligned to: " << sizeof(alignmentType(0)) << " bytes" << std::endl;
 
         /* managing and starting threads */
         std::vector<std::thread> threads;
@@ -78,26 +87,35 @@ public:
 
 int main(){
     /* get total execution time */
-    auto timer1(TimeMe("Total execution time"));
+    //auto timer1(TimeMe("Total execution time"));
 
     /* initialize container used for input data storage */
-    const int numElements = 200000000;
-    const int initialValue = 7;
+    const int numElements = 100000007;
+    const int initialValue = 3;
     std::vector<int> vect(numElements,initialValue);
 
     /* initialize aligned accumulator, run it and time it */
     {
         FalseSharingAccumulator<AlignedType<int>, int> accumulator;
         auto timer(TimeMe("Multithreaded accumulate"));
-        if ( accumulator.elementwiseSum(vect) == 567 ) std::cout << "Guessed it! Lucky me!"; // using the output so that the compiler does not optimize too much
+        if ( accumulator.elementwiseSum(vect) != initialValue * numElements ) std::cout << "Smth went wrong";
     }
 
     /* initialize unaligned accumulator, run it and time it */
     {
         FalseSharingAccumulator<UnalignedType<int>, int> accumulator;
-        auto timer(TimeMe("Multithreaded accumulate"));
-        if ( accumulator.elementwiseSum(vect) == 567 ) std::cout << "Guessed it! Lucky me!"; // using the output so that the compiler does not optimize too much
+        auto timer(TimeMe("Multi-threaded accumulate"));
+        if ( accumulator.elementwiseSum(vect) != initialValue * numElements ) std::cout << "Smth went wrong";
     }
+
+    {
+		auto timer(TimeMe("\nSingle-threaded custom accumulate"));
+		if (accumulate(vect) != initialValue * numElements ) std::cout << "Smth went wrong";
+	}
+    {
+		auto timer(TimeMe("Single-threaded std::accumulate"));
+		if (std::accumulate(vect.begin(), vect.end(), 0) != initialValue * numElements ) std::cout << "Smth went wrong";
+	}
 
     return 0;
 }
