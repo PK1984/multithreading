fbe616b404392a8a297f2a2d8ed073a4a27cb0a4
vboxuser <vboxuser@Debian.myguest.virtualbox.org>
2023-01-28 14:32:17 +0100

    .gitignore

---
+++ IntroductionToThreads/destruction/destruction.cpp
@@ -0,0 +1,21 @@
+#include <thread>
+#include <stdexcept>
+#include <iostream>
+ 
+int main()
+{
+    {
+        std::thread th([](){});
+        std::cout << "I have reached the end of the block no 1." << std::endl;
+    } // at the end of the block the destructor of th object will be called.
+      // It has been neither joined nor detached. The destructor will call std::terminate
+ 
+ 
+    std::thread th([](){});
+    {
+        throw std::runtime_error("An error occured");
+        th.join();                                 
+        std::cout << "I have reached the end of the block no 2." << std::endl;
+    }  // after throwing an exception destructor of th object will be called,
+       //  the following join() will be skipped
+}
