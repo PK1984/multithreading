760de70b9f245c36d1670f11f7fe83f1bcb2ab02
vboxuser <vboxuser@Debian.myguest.virtualbox.org>
2023-02-17 08:33:41 +0100

    recursive_mutex.cpp cleanup

--- Mutexes/recursive_mutex.cpp
+++ Mutexes/recursive_mutex.cpp
@@ -1,58 +1,45 @@
 #include <iostream>
 #include <mutex>
+#include <thread>
 
 class BankAccount {
 public:
-    BankAccount(int initialBalance = 0) : balance_(initialBalance) {}
-    int getBalance() const   {
-		std::lock_guard<std::recursive_mutex> lock(mtx_);
-		return balance_;
-	}
+    BankAccount(int initialBalance = 0) : balance(initialBalance) {}
+    int getBalance() const {
+        std::lock_guard<std::recursive_mutex> lock(mtx);
+        return balance;
+    }
     void deposit(int amount) {
-		std::lock_guard<std::recursive_mutex> lock(mtx_);
-		balance_ += amount;
-	}
+        std::lock_guard<std::recursive_mutex> lock(mtx);
+        balance += amount;
+    }
     bool withdraw(int amount) {
-        std::lock_guard<std::recursive_mutex> lock(mtx_);
-        if (balance_ >= amount) {
-            balance_ -= amount;
+        std::lock_guard<std::recursive_mutex> lock(mtx);
+        if (balance >= amount) {
+            balance -= amount;
             return true;
         } else {
             return false;
         }
     }
+ 
     bool transfer(BankAccount& other, int amount) {
-        std::lock_guard<std::recursive_mutex> lock(mtx_);
-        if (balance_ >= amount) {
-            balance_ -= amount;
+        std::lock_guard<std::recursive_mutex> lock(mtx);
+        if (withdraw(amount)) {
             other.deposit(amount);
             return true;
         } else {
             return false;
         }
     }
+ 
 private:
-    int balance_;
-    mutable std::recursive_mutex mtx_;
+    int balance;
+    mutable std::recursive_mutex mtx;
 };
 
 int main() {
     BankAccount a(100), b(50);
-
-    std::cout << "Initial balances: a=" << a.getBalance() << ", b=" << b.getBalance() << std::endl;
-
-    // Deposit 50 into account a
-    a.deposit(50);
-    std::cout << "After depositing 50 into a: a=" << a.getBalance() << ", b=" << b.getBalance() << std::endl;
-
-    // Withdraw 30 from account a
-    if (a.withdraw(30)) {
-        std::cout << "After withdrawing 30 from a: a=" << a.getBalance() << ", b=" << b.getBalance() << std::endl;
-    } else {
-        std::cout << "Failed to withdraw 30 from a (not enough balance)" << std::endl;
-    }
-
-    // Transfer 20 from account a to account b
     if (a.transfer(b, 20)) {
         std::cout << "After transferring 20 from a to b: a=" << a.getBalance() << ", b=" << b.getBalance() << std::endl;
     } else {
